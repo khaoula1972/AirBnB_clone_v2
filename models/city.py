d63bd96e286973b7eea666039e4be541de1187ff
Aziz <ngwk.zz@outlook.com>
2023-11-22 19:46:09 +0300

    DBStorage - States and Cities
    Co-authored-by: Khaoula <khaoula.nakkach@gmail.com>

--- models/city.py
+++ models/city.py
@@ -2,23 +2,24 @@
 """
 This conatins the class city
 """
-from models.base_model import BaseModel
-# from sqlalchemy import Column, String, ForeignKey
-# from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """
     This is City class that inherets from BaseModel
     """
 
+    """
     state_id = ""
     name = ""
-
     """
+
     __tablename__ = 'cities'
 
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
 
-    places = relationship("Place", backref="cities", cascade="delete")"""
+    places = relationship("Place", backref="cities", cascade="delete")
